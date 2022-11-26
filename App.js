ac5bc9458d29cbbc113c8c7aff0432af2551fd14
priyanshupandey-pp <80607973+priyanshupandey-pp@users.noreply.github.com>
2022-11-26 13:23:01 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,9 @@
+import * as React from 'react';
+import { Text, View, Button,StyleSheet } from 'react-native';
+import PickImage from './screens/Camera'
+
+export default function App() {
+  return (
+    <PickImage />
+  );
+}
